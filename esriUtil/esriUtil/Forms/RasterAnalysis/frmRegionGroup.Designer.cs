8b1fef6f88328d60bd1b3dd9224330ba28c916a5
jshogland <jshogland@f992fb60-5967-ae4a-bb26-c202bc534270>
2012-07-03 18:14:59 +0000

    forgot Region Group

---
+++ esriUtil/esriUtil/Forms/RasterAnalysis/frmRegionGroup.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace esriUtil.Forms.RasterAnalysis
+{
+    partial class frmRegionGroup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegionGroup));
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.txtOutName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbInRaster1 = new System.Windows.Forms.ComboBox();
+            this.btnOpenRaster = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnWorkspace = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtWorkspace = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(224, 115);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(54, 24);
+            this.btnExecute.TabIndex = 47;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // txtOutName
+            // 
+            this.txtOutName.Location = new System.Drawing.Point(10, 118);
+            this.txtOutName.Name = "txtOutName";
+            this.txtOutName.Size = new System.Drawing.Size(205, 20);
+            this.txtOutName.TabIndex = 45;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Input Raster";
+            // 
+            // cmbInRaster1
+            // 
+            this.cmbInRaster1.FormattingEnabled = true;
+            this.cmbInRaster1.Location = new System.Drawing.Point(10, 26);
+            this.cmbInRaster1.Name = "cmbInRaster1";
+            this.cmbInRaster1.Size = new System.Drawing.Size(236, 21);
+            this.cmbInRaster1.TabIndex = 42;
+            // 
+            // btnOpenRaster
+            // 
+            this.btnOpenRaster.Image = global::esriUtil.Properties.Resources.cmdOpenProject;
+            this.btnOpenRaster.Location = new System.Drawing.Point(251, 22);
+            this.btnOpenRaster.Name = "btnOpenRaster";
+            this.btnOpenRaster.Size = new System.Drawing.Size(27, 27);
+            this.btnOpenRaster.TabIndex = 41;
+            this.btnOpenRaster.UseVisualStyleBackColor = true;
+            this.btnOpenRaster.Click += new System.EventHandler(this.btnOpenRaster_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 46;
+            this.label4.Text = "Out Name";
+            // 
+            // btnWorkspace
+            // 
+            this.btnWorkspace.Image = global::esriUtil.Properties.Resources.cmdOpenProject;
+            this.btnWorkspace.Location = new System.Drawing.Point(251, 68);
+            this.btnWorkspace.Name = "btnWorkspace";
+            this.btnWorkspace.Size = new System.Drawing.Size(27, 27);
+            this.btnWorkspace.TabIndex = 50;
+            this.btnWorkspace.UseVisualStyleBackColor = true;
+            this.btnWorkspace.Click += new System.EventHandler(this.btnWorkspace_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 49;
+            this.label1.Text = "Out Workspace";
+            // 
+            // txtWorkspace
+            // 
+            this.txtWorkspace.Enabled = false;
+            this.txtWorkspace.Location = new System.Drawing.Point(10, 72);
+            this.txtWorkspace.Name = "txtWorkspace";
+            this.txtWorkspace.Size = new System.Drawing.Size(233, 20);
+            this.txtWorkspace.TabIndex = 48;
+            // 
+            // frmRegionGroup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 154);
+            this.Controls.Add(this.btnWorkspace);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWorkspace);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtOutName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbInRaster1);
+            this.Controls.Add(this.btnOpenRaster);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmRegionGroup";
+            this.Text = "Region Group";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox txtOutName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbInRaster1;
+        private System.Windows.Forms.Button btnOpenRaster;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnWorkspace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWorkspace;
+    }
+}
